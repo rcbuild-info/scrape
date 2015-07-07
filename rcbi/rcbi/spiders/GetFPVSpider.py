f1f18afbc62846af0f4dab3cd4c3b64bad0fe60f
Scott Shawcroft <scott.shawcroft@gmail.com>
2015-07-07 13:05:18 -0700

    Standardize some manufacturer names and do better name stripping.

--- rcbi/rcbi/spiders/GetFPVSpider.py
+++ rcbi/rcbi/spiders/GetFPVSpider.py
@@ -4,11 +4,21 @@ from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.contrib.linkextractors import LinkExtractor
 from rcbi.items import Part
 
+import string
+
+CORRECT = {"BlackOut": "Blackout",
+           "BobSmithIndustries": "Bob Smith Industries",
+           "FeiyuTech": "Feiyu Tech",
+           "LawMate": "Lawmate",
+           "Skylark": "Skylark FPV",
+           "SunnySky": "Sunnysky",
+           "Tiger Motors": "T-Motor"}
+STRIP_PREFIX = {"Tiger Motors": ["Tiger Motor", "Tiger"]}
 class GetFPVSpider(CrawlSpider):
     name = "getfpv"
     allowed_domains = ["getfpv.com"]
     start_urls = ["http://www.getfpv.com/catalog/seo_sitemap/product/"]
-    
+
     rules = (
         # Extract links matching 'category.php' (but not matching 'subsection.php')
         # and follow links from them (since no callback means follow=True by default).
@@ -17,7 +27,7 @@ class GetFPVSpider(CrawlSpider):
         # Extract links matching 'item.php' and parse them with the spider's method parse_item
         Rule(LinkExtractor(allow=('.*html', )), callback='parse_item'),
     )
-    
+
     def parse_item(self, response):
         headers = response.css("#product-attribute-specs-table th")
         data = response.css("#product-attribute-specs-table td")
@@ -35,4 +45,19 @@ class GetFPVSpider(CrawlSpider):
         if not product_name:
             return
         item["name"] = product_name[0].xpath("//h1/text()").extract()[0]
+        if "manufacturer" in item:
+            m = item["manufacturer"]
+            if m in STRIP_PREFIX:
+              for prefix in STRIP_PREFIX[m]:
+                if item["name"].startswith(prefix):
+                  item["name"] = item["name"][len(prefix):]
+            if m in CORRECT:
+              item["manufacturer"] = CORRECT[m]
+              if item["name"].startswith(CORRECT[m]):
+                item["name"] = item["name"][len(CORRECT[m]):]
+            if item["name"].startswith(m):
+              item["name"] = item["name"][len(m):]
+            item["name"] = item["name"].rstrip(string.whitespace).lstrip(string.whitespace + string.punctuation)
+            if m == "GetFPV Affiliate":
+              item.pop("manufacturer", None)
         return item
