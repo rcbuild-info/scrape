89b1f74e02e41606b0074f611ca57bb92c3bc883
Scott Shawcroft <scott.shawcroft@gmail.com>
2015-09-25 18:57:37 -0700

    Remove cPath from Urls.

--- rcbi/rcbi/spiders/MyRCMart.py
+++ rcbi/rcbi/spiders/MyRCMart.py
@@ -4,6 +4,9 @@ from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 from rcbi.items import Part
 
+import urlparse
+import urllib
+
 MANUFACTURERS = ["Walkera", "WL Toys", "Syma", "RCX"]
 CORRECT = {"RCXHOBBY.COM": "RCX", "Fatshark": "FatShark", "HobbyWing": "Hobbywing"}
 NEW_PREFIX = {}
@@ -22,11 +25,23 @@ class MyRCMartSpider(CrawlSpider):
     def parse_item(self, response):
       item = Part()
       item["site"] = self.name
-      item["url"] = response.url
       product_name = response.css("td.pageHeading")
       if not product_name:
           return
       item["name"] = product_name[0].xpath("text()").extract()[0]
+
+      parsed = urlparse.urlparse(response.url)
+      qs = urlparse.parse_qs(parsed.query)
+      qs.pop("cPath", None)
+      # By default all values in qs will be a list and cause funky % encoding
+      # in the resulting url. So, if the list is one value then we replace the
+      # list with it.
+      for k in qs:
+          if len(qs[k]) == 1:
+              qs[k] = qs[k][0]
+      item["url"] = urlparse.urlunparse((parsed[0], parsed[1], parsed[2],
+                                         parsed[3], urllib.urlencode(qs),
+                                         parsed[5]))
 
       details = response.css("td.prod_detail")
       manufacturer = None
