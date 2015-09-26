4ad9526f26f4eefe52b93ae0a04d86db5ec19ad1
Scott Shawcroft <scott.shawcroft@gmail.com>
2015-09-26 13:36:18 -0700

    v2 parts from Shendrones

--- rcbi/rcbi/spiders/Shendrones.py
+++ rcbi/rcbi/spiders/Shendrones.py
@@ -4,6 +4,12 @@ from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 from rcbi.items import Part
 
+import copy
+import json
+import re
+
+VARIANT_JSON_REGEX = re.compile("product: ({.*}),")
+
 class ShendronesSpider(CrawlSpider):
     name = "shendrones"
     allowed_domains = ["shendrones.myshopify.com"]
@@ -16,15 +22,48 @@ class ShendronesSpider(CrawlSpider):
     def parse_item(self, response):
       item = Part()
       item["site"] = self.name
-      item["url"] = response.url
-      product_name = response.css("h1[itemprop='name']")
-      if not product_name:
-          return
-      item["name"] = product_name[0].xpath("text()").extract()[0].strip()
 
-      price = response.css(".productPrice")
-      if price:
-        item["price"] = price.xpath("text()").extract()[0]
+      variant = {}
+      variant["timestamp"] = response.headers["Date"]
+      if "Last-Modified" in response.headers:
+        variant["timestamp"] = response.headers["Last-Modified"]
+      item["variants"] = [variant]
+      base_url = response.url
 
       item["manufacturer"] = "Shendrones"
-      return item
+
+      # Find the json info for variants.
+      body = response.body_as_unicode()
+
+      m = VARIANT_JSON_REGEX.search(body)
+      if m:
+        shopify_info = json.loads(m.group(1))
+        global_title = shopify_info["title"]
+        preorder = False
+        if global_title.endswith("Pre Order"):
+          global_title = global_title[:-len("Pre Order")].strip()
+          variant["stock_state"] = "backordered"
+          preorder = True
+        for v in shopify_info["variants"]:
+          if v["title"] != "Default Title":
+            item["name"] = global_title + " " + v["title"]
+            variant["url"] = base_url + "?variant=" + str(v["id"])
+          else:
+            item["name"] = global_title
+            variant["url"] = base_url
+          variant["price"] = "${:.2f}".format(v["price"] / 100)
+          if not preorder:
+            if v["inventory_quantity"] <= 0:
+              if v["inventory_policy"] == "deny":
+                variant["stock_state"] = "out_of_stock"
+              else:
+                variant["stock_state"] = "backordered"
+            elif v["inventory_quantity"] < 3:
+              variant["stock_state"] = "low_stock"
+              variant["stock_text"] = "Only " + str(v["inventory_quantity"]) + " left!"
+            else:
+              variant["stock_state"] = "in_stock"
+
+          yield item
+          item = copy.deepcopy(item)
+          variant = item["variants"][0]
