eed3f21f6300330d4a1354e43cc2adb7d3345167
Scott Shawcroft <scott.shawcroft@gmail.com>
2015-09-26 18:16:51 -0700

    v2 parts from FPVReconn. Logic mostly lifted from RotorGeeks.

--- rcbi/rcbi/spiders/FpvReconn.py
+++ rcbi/rcbi/spiders/FpvReconn.py
@@ -10,6 +10,9 @@ import urllib
 MANUFACTURERS = ["Gemfan", "HQprop", "Cobra", "SunnySky", "FPV-Reconn"]
 CORRECT = {"HQprop": "HQProp", "SunnySky": "Sunnysky"}
 NEW_PREFIX = {}
+STOCK_STATE_MAP = {"In Stock": "in_stock",
+                   "Out Of Stock": "out_of_stock",
+                   "Pre-Order": "backordered"}
 class FPVReconnSpider(CrawlSpider):
     name = "fpvreconn"
     allowed_domains = ["fpv-reconn.com"]
@@ -22,8 +25,16 @@ class FPVReconnSpider(CrawlSpider):
     )
 
     def parse_item(self, response):
+      product_name = response.css("#content h1")
+      if not product_name:
+          return
+
       item = Part()
       item["site"] = self.name
+
+      variant = {}
+      variant["timestamp"] = response.headers["Date"]
+      item["variants"] = [variant]
 
       parsed = urlparse.urlparse(response.url)
       qs = urlparse.parse_qs(parsed.query)
@@ -34,18 +45,34 @@ class FPVReconnSpider(CrawlSpider):
       for k in qs:
           if len(qs[k]) == 1:
               qs[k] = qs[k][0]
-      item["url"] = urlparse.urlunparse((parsed[0], parsed[1], parsed[2],
-                                         parsed[3], urllib.urlencode(qs),
-                                         parsed[5]))
+      variant["url"] = urlparse.urlunparse((parsed[0], parsed[1], parsed[2],
+                                            parsed[3], urllib.urlencode(qs),
+                                            parsed[5]))
 
-      product_name = response.css("#content h1")
-      if not product_name:
-          return
+
       item["name"] = product_name[0].xpath("text()").extract()[0].strip()
 
       price = response.css(".price")
       if price:
-        item["price"] = price.xpath("text()").extract()[0][len("Price:"):].strip()
+        variant["price"] = price.xpath("text()").extract()[0][len("Price:"):].strip()
+
+      text = response.css(".description::text")
+      headers = response.css(".description span")
+      # Skip the first. Its a newline before the first span.
+      text_index = 1
+      for h in headers:
+        label = h.css("::text").extract_first().strip()
+        value = text[text_index].extract().strip()
+        text_index += 2
+
+        if label == "Availability:":
+          variant["stock_text"] = value
+          if value in STOCK_STATE_MAP:
+            variant["stock_state"] = STOCK_STATE_MAP[value]
+          else:
+            print(value)
+        elif label == "Product Code:":
+          item["sku"] = value
 
       for m in MANUFACTURERS:
         if item["name"].startswith(m):
