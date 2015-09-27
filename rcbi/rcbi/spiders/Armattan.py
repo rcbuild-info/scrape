eb88a68856cf77cd451bcb34d821daf465ef07e6
Scott Shawcroft <scott.shawcroft@gmail.com>
2015-09-26 20:23:03 -0700

    scrape v2 parts from Armattan

--- rcbi/rcbi/spiders/Armattan.py
+++ rcbi/rcbi/spiders/Armattan.py
@@ -20,7 +20,6 @@ class ArmattanSpider(CrawlSpider):
     def parse_item(self, response):
       item = Part()
       item["site"] = self.name
-      item["url"] = response.url
       product_name = response.css(".DetailRow")
       if not product_name:
           return
@@ -29,9 +28,25 @@ class ArmattanSpider(CrawlSpider):
       if item["name"].startswith("Armattan"):
         item["name"] = item["name"][len("Armattan"):].strip()
 
+      variant = {}
+      variant["timestamp"] = response.headers["Date"]
+      item["variants"] = [variant]
+      variant["url"] = response.url
+
       price = response.css(".ProductPrice")
       if price:
-        item["price"] = price.xpath("text()").extract()[0]
+        variant["price"] = price.xpath("text()").extract()[0]
+
+      add_to_cart = response.css(".BulkDiscount::attr(style)")
+      if add_to_cart:
+        style = add_to_cart.extract_first().strip()
+        if style == "display:":
+          variant["stock_state"] = "in_stock"
+        else:
+          variant["stock_state"] = "out_of_stock"
+
+      # TODO(tannewt): Handle variants. It'll be tricky because selecting a
+      # radio button triggers an AJAX call.
 
       item["manufacturer"] = "Armattan"
       return item
