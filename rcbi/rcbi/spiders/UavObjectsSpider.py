30517494aa02f6b9f63817030c527cf6c5614a84
Scott Shawcroft <scott.shawcroft@gmail.com>
2015-09-26 20:44:51 -0700

    Scrape v2 parts from uavobjects

--- rcbi/rcbi/spiders/UavObjectsSpider.py
+++ rcbi/rcbi/spiders/UavObjectsSpider.py
@@ -6,6 +6,9 @@ from rcbi.items import Part
 
 MANUFACTURERS = ["Rctimer", "RCTimer", "BaseCam", "Elgae", "ELGAE", "ArduFlyer", "Boscam"]
 CORRECT = {"Rctimer": "RCTimer", "ELGAE": "Elgae"}
+STOCK_STATE_MAP = {"available-on-backorder": "backordered",
+                   "in-stock": "in_stock",
+                   "out-of-stock": "out_of_stock"}
 class UavObjectsSpider(SitemapSpider):
     name = "uavobjects"
     allowed_domains = ["uavobjects.com"]
@@ -18,11 +21,30 @@ class UavObjectsSpider(SitemapSpider):
     def parse_item(self, response):
         item = Part()
         item["site"] = "uavobjects"
-        item["url"] = response.url
         product_name = response.css("h1.product_title")
         if not product_name:
             return
         item["name"] = product_name[0].xpath("text()").extract()[0]
+
+        variant = {}
+        variant["timestamp"] = response.headers["Date"]
+        item["variants"] = [variant]
+        variant["url"] = response.url
+
+        price = response.css("[itemprop=\"price\"]::attr(content)")
+        if price:
+          price = float(price.extract_first())
+          variant["price"] = "${:.2f}".format(price)
+
+        stock = response.css(".stock")
+        if stock:
+          c = stock.css("::attr(class)").extract_first().split()[-1]
+          if c in STOCK_STATE_MAP:
+            variant["stock_state"] = STOCK_STATE_MAP[c]
+            variant["stock_text"] = stock.css("::text").extract_first().strip()
+          else:
+            print(c)
+
         for m in MANUFACTURERS:
           if item["name"].startswith(m):
             if m in CORRECT:
