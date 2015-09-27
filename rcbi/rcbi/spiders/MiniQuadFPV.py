73caff371a8cbf5c95156207dbc68562bd513c4e
Scott Shawcroft <scott.shawcroft@gmail.com>
2015-09-27 13:13:30 -0700

    Scrape v2 parts from miniquad fpv.

--- rcbi/rcbi/spiders/MiniQuadFPV.py
+++ rcbi/rcbi/spiders/MiniQuadFPV.py
@@ -7,6 +7,9 @@ from rcbi.items import Part
 MANUFACTURERS = []
 CORRECT = {}
 NEW_PREFIX = {}
+
+STOCK_STATE_MAP = {"http://schema.org/InStock": "in_stock",
+                   "http://schema.org/OutOfStock": "out_of_stock"}
 class MiniQuadFPVSpider(CrawlSpider):
     name = "miniquadfpv"
     allowed_domains = ["miniquadfpv.com"]
@@ -20,15 +23,32 @@ class MiniQuadFPVSpider(CrawlSpider):
     def parse_item(self, response):
       item = Part()
       item["site"] = self.name
-      item["url"] = response.url
       product_name = response.css(".product_title")
       if not product_name:
           return
       item["name"] = product_name[0].xpath("text()").extract()[0].strip()
 
+      item["manufacturer"] = "THug FRAMES"
+
+      sku = response.css("[itemprop=\"sku\"]::text")
+      if sku:
+        item["sku"] = sku.extract_first().strip()
+
+      variant = {}
+      variant["timestamp"] = response.headers["Date"]
+      item["variants"] = [variant]
+      variant["url"] = response.url
+
       price = response.css(".price .amount")
       if price:
-        item["price"] = price.xpath("text()").extract()[0]
+        variant["price"] = price.xpath("text()").extract()[0]
 
-      item["manufacturer"] = "THug FRAMES"
+      availability = response.css("[itemprop=\"availability\"]::attr(href)").extract_first()
+      if availability:
+        variant["stock_state"] = STOCK_STATE_MAP[availability]
+        if item["name"].endswith("PRE ORDER"):
+          item["name"] = item["name"][:-len("PRE ORDER")].strip(u" \u2013")
+          variant["stock_state"] = "backordered"
+          variant["stock_text"] = "PRE ORDER"
+
       return item
