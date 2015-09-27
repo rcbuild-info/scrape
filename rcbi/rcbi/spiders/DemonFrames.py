871d231363b52f78f1c387f02263c2dc71d91b67
Scott Shawcroft <scott.shawcroft@gmail.com>
2015-09-27 11:59:39 -0700

    Scrape v2 parts from Demon Frames.

--- rcbi/rcbi/spiders/DemonFrames.py
+++ rcbi/rcbi/spiders/DemonFrames.py
@@ -12,6 +12,8 @@ MANUFACTURERS = []
 CORRECT = {"DEMON": "Demon Frames"}
 MANUFACTURERS.extend(CORRECT.keys())
 QUANTITY = {u"PACK OF 10 – ": 10}
+STOCK_STATE_MAP = {"http://schema.org/InStock": "in_stock",
+                   "http://schema.org/OutOfStock": "out_of_stock"}
 class DemonFramesSpider(CrawlSpider):
     name = "demonframes"
     allowed_domains = ["demonframes.com"]
@@ -26,20 +28,31 @@ class DemonFramesSpider(CrawlSpider):
     def parse_item(self, response):
       item = Part()
       item["site"] = self.name
-      item["url"] = response.url
       product_name = response.css("[itemprop='name']")
       if not product_name:
           return
       item["name"] = product_name[0].xpath("text()").extract()[0].strip()
 
-      price = response.css(".price .amount")
+      variant = {}
+      variant["timestamp"] = response.headers["Date"]
+      item["variants"] = [variant]
+      variant["url"] = response.url
+
+      price = response.css("[itemprop=\"price\"]::attr(content)")
       if price:
-        item["price"] = price.xpath("text()").extract()[0].strip()
+        variant["price"] = "${:.2f}".format(float(price.extract_first()))
 
       for quantity in QUANTITY:
         if quantity in item["name"]:
-          item["quantity"] = QUANTITY[quantity]
+          variant["quantity"] = QUANTITY[quantity]
           item["name"] = item["name"].replace(quantity, "").strip()
+
+      availability = response.css("[itemprop=\"availability\"]::attr(href)").extract_first()
+      if availability:
+        variant["stock_state"] = STOCK_STATE_MAP[availability]
+        text = response.css("p.stock::text")
+        if text:
+          variant["stock_text"] = text.extract_first()
 
       for m in MANUFACTURERS:
         if item["name"].startswith(m):
