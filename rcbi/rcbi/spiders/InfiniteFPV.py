06a894f0d6f40d2bc4a98c469c7abe64e1c8ee00
Scott Shawcroft <scott.shawcroft@gmail.com>
2015-09-27 11:26:19 -0700

    Scrape v2 parts from infiniteFPV

--- rcbi/rcbi/spiders/InfiniteFPV.py
+++ rcbi/rcbi/spiders/InfiniteFPV.py
@@ -4,6 +4,9 @@ from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 from rcbi.items import Part
 
+STOCK_STATE_MAP = {"available-on-backorder": "backordered",
+                   "in-stock": "in_stock",
+                   "out-of-stock": "out_of_stock"}
 class InfiniteFPVSpider(CrawlSpider):
     name = "infinitefpv"
     allowed_domains = ["infinitefpv.com"]
@@ -17,15 +20,28 @@ class InfiniteFPVSpider(CrawlSpider):
     def parse_item(self, response):
       item = Part()
       item["site"] = self.name
-      item["url"] = response.url
       product_name = response.css("[itemprop='name']")
       if not product_name:
           return
-      item["name"] = product_name[0].xpath("//h1/text()").extract()[0].strip()
+      item["name"] = product_name[0].xpath("//h1/text()").extract_first().strip()
+
+      variant = {}
+      variant["timestamp"] = response.headers["Date"]
+      item["variants"] = [variant]
+      variant["url"] = response.url
 
       price = response.css(".price .amount")
       if price:
-        item["price"] = price.xpath("text()").extract()[0]
+        variant["price"] = price.css("::text").extract_first()
+
+      stock = response.css(".stock")
+      if stock:
+        c = stock.css("::attr(class)").extract_first().split()[-1]
+        if c in STOCK_STATE_MAP:
+          variant["stock_state"] = STOCK_STATE_MAP[c]
+          variant["stock_text"] = stock.css("::text").extract_first().strip()
+        else:
+          print(c)
 
       item["manufacturer"] = "infiniteFPV"
       return item
