89176e6f138a84c627c9d6f63850e74f30476859
Scott Shawcroft <scott.shawcroft@gmail.com>
2015-09-27 21:28:26 -0700

    Scrape v2 parts from Hobby King.

--- rcbi/rcbi/spiders/HobbyKing.py
+++ rcbi/rcbi/spiders/HobbyKing.py
@@ -7,9 +7,11 @@ from rcbi.items import Part
 
 import re
 
-WAREHOUSE_RE=re.compile("\( ?(RU|USA|US|AR|AUS|AU|EU|UK) [Ww]arehouse ?\)")
-MANUFACTURERS = ["Turnigy", "Walkera", "ZTW", "ZIPPY", "Zippy", "TURNIGY", "Rhino", "Hobbywing", "OrangeRX", "OrangeRx", "HobbyKing", "Hobbyking", u"Hobbyking™", u"HobbyKing™", u"Hobbyking® ™", u"HobbyKing® ™", u"Hobbyking®", u"HobbyKing®", "Hobby King", "Goteck", "Futaba", "Flycolor", "Corona", "Durafly™"]
-CORRECT = {"ZIPPY": "Zippy", "TURNIGY": "Turnigy", "OrangeRX": "OrangeRx", "Hobbyking": "HobbyKing", u"Hobbyking™": "HobbyKing", u"HobbyKing™": "HobbyKing", u"Hobbyking® ™": "HobbyKing", u"HobbyKing® ™" : "HobbyKing", u"Hobbyking®" : "HobbyKing", u"HobbyKing®" : "HobbyKing", "Hobby King": "HobbyKing", "Durafly™": "Durafly"}
+WAREHOUSE_RE=re.compile("\( ?(RU|USA|US|AR|AUS|AU|EU|UK|NL) [Ww]arehouse ?\)")
+MANUFACTURERS = ["Turnigy", "Walkera", "ZTW", "Zippy", "Rhino", "Hobbywing", "OrangeRx", "HobbyKing", "Goteck", "Futaba", "Flycolor", "Corona"]
+CORRECT = {"ZIPPY": "Zippy", "TURNIGY": "Turnigy", "OrangeRX": "OrangeRx", "Hobbyking": "HobbyKing", u"Hobbyking™": "HobbyKing", u"HobbyKing™": "HobbyKing", u"Hobbyking® ™": "HobbyKing", u"HobbyKing® ™" : "HobbyKing", u"Hobbyking®" : "HobbyKing", u"HobbyKing®" : "HobbyKing", "Hobby King": "HobbyKing", u"Durafly™": "Durafly"}
+MANUFACTURERS.extend(CORRECT)
+MANUFACTURERS.sort(key=lambda x: len(x), reverse=True)
 NEW_PREFIX = {}
 class HobbyKingSpider(CrawlSpider):
     name = "hobbyking"
@@ -26,10 +28,15 @@ class HobbyKingSpider(CrawlSpider):
     def parse_item(self, response):
       item = Part()
       item["site"] = self.name
-      item["url"] = response.url
       product_name = response.css("#productDescription")
       if not product_name:
           return
+
+      variant = {}
+      variant["timestamp"] = response.headers["Date"]
+      item["variants"] = [variant]
+
+      variant["url"] = response.url
       item["name"] = product_name[0].xpath("text()").extract()[0]
       suffix = WAREHOUSE_RE.search(item["name"])
       if suffix != None:
@@ -39,16 +46,34 @@ class HobbyKingSpider(CrawlSpider):
       details = response.xpath("//*[@class=\"data_tbl\"]/table/tr/td/table/tr[2]/td[2]/table/tr/td[1]/span/table/tr/td[2]/text()")
       manufacturer = None
       for i, header in enumerate(headers):
-          header = header.extract()
-          if len(header) == 0:
-            continue
-          data = details[i].extract()
-          if header == "Weight (g)":
-            item["weight"] = data
+        header = header.extract()
+        if len(header) == 0:
+          continue
+        data = details[i].extract()
+        if header == "Weight (g)":
+          item["weight"] = data
 
       price = response.css("#price_lb")
       if price:
-        item["price"] = price[0].xpath("text()").extract()[0]
+        variant["price"] = price[0].xpath("text()").extract()[0].strip()
+
+      stock = response.css("#pstock2")
+      if stock:
+        q = stock[0].xpath("text()").extract()[0]
+        if q == "BK":
+          variant["stock_state"] = "backordered"
+        elif q == "0":
+          variant["stock_state"] = "out_of_stock"
+          variant["stock_text"] = q
+        else:
+          variant["stock_state"] = "in_stock"
+          variant["stock_text"] = q
+
+      sku = response.css("#desc_sku")
+      if sku:
+        item["sku"] = sku[0].xpath("text()").extract()[0]
+
+      variant["location"] = response.xpath("//*[@id=\"details\"]/div[1]/div[2]/div/table/tbody/tr[1]/td[2]/text()").extract()[0].strip()
 
       if not manufacturer:
         for m in MANUFACTURERS:
